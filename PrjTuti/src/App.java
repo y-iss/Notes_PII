d657b9a763924b9f0671e32538f9e65a2f4e87fe
y-iss <alegria.farinango@epn.edu.ec>
2024-05-22 12:15:24 -0500

    refactorizar

--- PrjTuti/src/App.java
+++ PrjTuti/src/App.java
@@ -1,12 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        // argumentosMain(args);
+
         Persona oP = new Persona();
         Persona oP2 = new Persona(null, null, null);
         Supervisor oS = new Supervisor();
 
-        oP.setCedula("1243");
-        oP.setNombre("Pepe");
-        // oP.setApellido("Lopez");
+        oP.setCedula("1234");
+        oP.setNombre("Isa");
+        oP.setApellido(null);
 
         System.out.println(oP.getCedula());
         System.out.println(oP.getNombre());
@@ -16,6 +19,17 @@ public class App {
         System.out.println(oP2.getNombre());
         System.out.println(oP2.getApellido());
 
+    }
+
+    private static void argumentosMain(String[] args) {
+        if ((args.length == 2) &&
+                (args[0].equals("y-iss") && args[1].equals("1234")))
+            System.out.println("Buen dia amo, estoy lista para trabajar");
+
+        else {
+            System.out.println("Detecto que eres un intruso...");
+            System.exit(0);
+        }
     }
 
 }
