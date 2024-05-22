58148065763d7753c3cc8782e44798c28fad2986
y-iss <alegria.farinango@epn.edu.ec>
2024-05-22 12:45:37 -0500

    Scanner

--- PrjTuti/src/App.java
+++ PrjTuti/src/App.java
@@ -1,7 +1,24 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import javax.sound.midi.Soundbank;
+
 public class App {
     public static void main(String[] args) throws Exception {
-
         // argumentosMain(args);
+        // FlujoDatoTeclado();
+
+        int total = 0;
+        String datos = "12-34-56";
+        Scanner sc = new Scanner(datos);
+        sc.useDelimiter("-");
+
+        while (sc.hasNextInt()) {
+            total += sc.nextInt();
+        }
+        System.out.println("total es: " + total);
+        sc.close();
 
         Persona oP = new Persona();
         Persona oP2 = new Persona(null, null, null);
@@ -21,10 +38,27 @@ public class App {
 
     }
 
+    private static void FlujoDatoTeclado() {
+        int total = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa numeros para sumar");
+        while (sc.hasNextInt()) {
+            total += sc.nextInt();
+        }
+        System.out.println("La suma de los numeros es: " + total);
+        sc.close();
+    }
+
     private static void argumentosMain(String[] args) {
         if ((args.length == 2) &&
-                (args[0].equals("y-iss") && args[1].equals("1234")))
+                (args[0].equals("y-iss") && args[1].equals("1234"))) {
+
+            var entrada = Arrays.toString(args);
+
             System.out.println("Buen dia amo, estoy lista para trabajar");
+            System.out.println("Los parametros que ingresaste son: " + args);
+            System.out.println("Los parametros que ingresaste son: " + entrada);
+        }
 
         else {
             System.out.println("Detecto que eres un intruso...");
