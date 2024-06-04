ee332124ff464124437083eb060759727207c00d
y-iss <alegria.farinango@epn.edu.ec>
2024-06-04 12:24:23 -0500

    revisar

--- prjLobo/src/App.java
+++ prjLobo/src/App.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class App {
     public static Scanner sc = new Scanner(System.in);
     public static Juego juego;
-    
+
     public static void main(String[] args) throws Exception {
-        juego = new Juego(); 
+        juego = new Juego();
         juego.jugarLobito();
-        //juego.moverBarca();
+        juego.moverBarca();
     }
 }
--- prjLobo/src/Juego.java
+++ prjLobo/src/Juego.java
@@ -23,28 +23,57 @@ public class Juego {
             individuo = (vikingoEstaIzq)
                     ? ladoIzq[opcMenu]
                     : ladoDer[opcMenu];
+            if (vikingoEstaIzq) {
+                individuo = ladoIzq[opcMenu];
+                ladoIzq[opcMenu] = " ";
+            } else {
+                individuo = ladoDer[opcMenu];
+                ladoDer[opcMenu] = individuo;
+            }
+            moverBarca(individuo);
+            vikingoEstaIzq = !vikingoEstaIzq;
+
+            if (vikingoEstaIzq) {
+                ladoIzq[opcMenu] = individuo;
+                setBarcaRio(1, " ");
+            } else {
+                ladoDer[opcMenu] = individuo;
+                setBarcaRio(rio.length(), " ");
+            }
+
+        } while (true);
+    }
+
+    // *verificarRegla() : String
+    private String verificarRegla() {
+        String msg = "";
+        // Reglas con las que pierde el juego
         if (vikingoEstaIzq) {
-            individuo       = ladoIzq[opcMenu];
-            ladoIzq[opcMenu] = " "; 
-        }
-        else{
-            individuo      = ladoDer[opcMenu];
-            ladoDer[opcMenu]; 
-        }
-        moverBarca(individuo);
-        vikingoEstaIzq = !vikingoEstaIzq;
+            // R1: lobo come caperucita
+            if (ladoDer[0].equals(" L ") && ladoDer[2].equals(" c "))
+                msg = "Marcho la caperuita ";
 
-        if(vikingoEstaIzq){
-            ladoIzq[opcMenu]   =  individuo;
-            setBarcaRio(1, " ");
+            // R2: caperucita come uvas
+            if (ladoDer[0].equals(" C ") && ladoDer[2].equals(" U "))
+                msg = "Marcho las uvas ";
+        } else {
+            // R1: lobo come caperucita
+            if (ladoIzq[0].equals(" L ") && ladoIzq[2].equals(" c "))
+                msg = "Marcho la caperuita ";
+
+            // R2: caperucita come uvas
+            if (ladoIzq[0].equals(" C ") && ladoIzq[2].equals(" U "))
+                msg = "Marcho las uvas ";
         }
-        else {
-            ladoDer[opcMenu] = individuo;
-            setBarcaRio(rio.length(), " ");
+        if (ladoDer[0].equals("V") && ladoDer[1].equals("L") && ladoDer[2].equals("C") && ladoDer[3].equals("U"))
+            msg = "Lo haz logrado";
+        if (msg.isEmpty()) {
+            System.out.println(msg);
+            System.exit(0);
         }
 
-    } while (true);
-}
+        return "";
+    }
 
     private Short mostrarMenu() {
         int opc = -1;
@@ -56,11 +85,19 @@ public class Juego {
                 + "\n 4 Salir  ");
         do {
             try {
-                opc = -1;
+                // opc = -1;
                 String personaje = " ";
                 System.out.print("\n[+] Ingresa una opc:");
                 opc = App.sc.nextInt();
-                // validar la opcion selecionada
+                // verificar que exista el personaje
+                personaje = (vikingoEstaIzq)
+                        ? ladoIzq[opc]
+                        : ladoDer[opc];
+                if (personaje.trim().isEmpty()) {
+                    opc = -1;
+                    System.out.println("No existe este personaje en el lado de la barca");
+                }
+
                 if (opc == 4) {
                     System.out.println("No huyas... cobarde..!");
                     System.exit(0);
@@ -92,6 +129,11 @@ public class Juego {
             Thread.sleep(100);
         } catch (InterruptedException e) {
         }
+    }
+
+    public void moverBarca() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moverBarca'");
     }
 }
 
