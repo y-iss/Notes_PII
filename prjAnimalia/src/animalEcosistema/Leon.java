0dd5f1f0b68f54ce8640c477d76db29bb4ce925e
y-iss <alegria.farinango@epn.edu.ec>
2024-06-19 12:55:54 -0500

    Leon

--- prjAnimalia/src/animalEcosistema/Leon.java
+++ prjAnimalia/src/animalEcosistema/Leon.java
@@ -1,5 +1,39 @@
 package animalEcosistema;
 
-public class Leon {
+import animalClasificacion.Mamifero;
+import animalEcosistema.comportamiento.IAccion1;
+import animalEcosistema.comportamiento.IAccionCarnivora;
+
+public class Leon extends Mamifero implements IAccion1, IAccionCarnivora {
+
+    public Leon(String nombre) {
+        setNombre(nombre);
+
+    }
+
+    @Override
+    public String toString() {
+        return "\n Clase : " + getClass().getName()
+                + "\n Nombre : " + getNombre();
+
+    }
+
+    @Override
+    public void comer() {
+
+        throw new UnsupportedOperationException("Unimplemented method 'comer'");
+    }
+
+    @Override
+    public void correr(Pato alimentoPatuno) {
+        
+        throw new 'correr'")
+    }
+
+    @Override
+    public void correr() {
+
+        throw new UnsupportedOperationException("Unimplemented method 'correr'");
+    }
 
 }
--- prjAnimalia/src/animalEcosistema/Pato.java
+++ prjAnimalia/src/animalEcosistema/Pato.java
@@ -1,5 +1,42 @@
 package animalEcosistema;
 
-public class Pato {
+import animalClasificacion.Ave;
+import animalEcosistema.comportamiento.IAccion1;
+import animalEcosistema.comportamiento.IAccion2;
+import animalEcosistema.comportamiento.IAccion3;
+
+public class Pato extends Ave implements IAccion1, IAccion2, IAccion3 {
+
+    public Halcon amigoHalcon;
+
+    public Pato(String nombre) {
+        setNombre(nombre);
+        amigoHalcon = new Halcon("halconsillo");
+    }
+
+    @Override
+    public String toString() {
+        return "\n Clase : " + getClass().getName()
+                + "\n Nombre : " + getNombre()
+                + "\n NroAlas : " + getNroAlas();
+    }
+
+    @Override
+    public void nadar() {
+
+        System.out.println("Pato esta nadando ....");
+    }
+
+    @Override
+    public void volar() {
+
+        System.out.println("Pato esta volando ....");
+    }
+
+    @Override
+    public void correr() {
+
+        System.out.println("Pato esta corriendo ....");
+    }
 
 }
